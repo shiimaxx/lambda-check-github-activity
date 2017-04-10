cfa15609d418aab9cd54ab1f8a6e081f04208846
shiimaxx <yoshima@shiimaxx.com>
2017-04-11 00:59:32 +0900

    Update

--- check_github_activity.py
+++ check_github_activity.py
@@ -1,12 +1,20 @@
+#!/usr/bin/env python3
+
 import datetime
+import os
+
 from pytz import timezone
 
 from github import Github
 
 
+GITHUB_USER = os.environ["GITHUB_USER"]
+GITHUB_PASSWORD = os.environ["GITHUB_PASSWORD"]
+
+
 if __name__ == "__main__":
     current_date = datetime.date.today()
-    git = Github("", "")
+    git = Github(GITHUB_USER, GITHUB_PASSWORD)
 
     for event in git.get_user("shiimaxx").get_events():
         if event.type == "PushEvent":
@@ -14,6 +22,7 @@ if __name__ == "__main__":
             break
 
     jst_created_at = timezone("Asia/Tokyo").localize(event.created_at)
+    recently_event_date = jst_created_at.date()
 
-    recently_event_time \
-        = datetime.datetime.strftime(jst_created_at, "%Y-%m-%d")
+    if current_date > recently_event_date:
+        print("Shut the fuck up and write some code.")
