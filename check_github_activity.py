3a43d6b3788e9f4911924f4e6b54ba584647b63b
shiimaxx <yoshima@shiimaxx.com>
2017-04-11 20:35:04 +0900

    Update

--- check_github_activity.py
+++ check_github_activity.py
@@ -1,26 +1,44 @@
 #!/usr/bin/env python3
 
 import datetime
+import json
 import os
+import urllib
 
 from github import Github
 
 
 GITHUB_USER = os.environ["GITHUB_USER"]
-GITHUB_PASSWORD = os.environ["GITHUB_PASSWORD"]
+GITHUB_LOGIN_USER = os.environ["GITHUB_LOGIN_USER"]
+GITHUB_LOGIN_PASSWORD = os.environ["GITHUB_LOGIN_PASSWORD"]
+SLACK_WEBHOOK_URL = os.environ["SLACK_WEBHOOK_URL"]
 
 
-if __name__ == "__main__":
+def to_slack(text):
+    json_data = json.dumps({"text": text}).encode("utf-8")
+    headers = {"Content-Type": "application/json"}
+    requests = urllib.request.Request(
+            SLACK_WEBHOOK_URL, data=json_data, headers=headers)
+
+    response = urllib.request.urlopen(requests)
+
+    return response
+
+
+def lambda_handler(event, context):
     current_date = datetime.date.today()
-    git = Github(GITHUB_USER, GITHUB_PASSWORD)
+    git = Github(GITHUB_LOGIN_USER, GITHUB_LOGIN_PASSWORD)
 
-    for event in git.get_user("shiimaxx").get_events():
-        if event.type == "PushEvent":
-            recently_event = event
+    for github_event in git.get_user(GITHUB_USER).get_events():
+        if github_event.type == "PushEvent":
+            recently_github_event = github_event
             break
 
-    jst_created_at = event.created_at + datetime.timedelta(hours=9)
+    jst_created_at = \
+        recently_github_event.created_at + datetime.timedelta(hours=9)
     recently_event_date = jst_created_at.date()
 
-    if current_date > recently_event_date:
-        print("Shut the fuck up and write some code.")
+    if current_date == recently_event_date:
+        response = to_slack("Shut the fuck up and write some code.")
+
+    return response.status
